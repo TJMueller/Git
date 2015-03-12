b40f7f964f74c258664f3cb9cef4b4dbf40a605e
TJMueller <timothyjonmueller@gmail.com>
2015-03-11 19:55:56 -0500

    Initial Commit

---
+++ Git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git
+//
+//  Created by Timothy Mueller on 3/11/15.
+//  Copyright (c) 2015 Timothy Mueller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
